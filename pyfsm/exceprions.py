5f7004fe18cc5b41327b9988b685bedd31de2c04
Dmitry Kotov <dmitrii.kotov@fogstream.ru>
2019-05-30 17:39:39 +1000

    * New exception format * Tests

--- pyfsm/exceprions.py
+++ pyfsm/exceprions.py
@@ -1,2 +1,20 @@
+import json
+
+
 class UnreleasedTransition(Exception):
     """It reports that the state transition cannot be performed."""
+
+    def __init__(self, message: str = None, errors: object = None) -> None:
+        self.message = message
+        self.errors = errors
+        args = [message] if message else []
+        super(UnreleasedTransition, self).__init__(*args)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    def to_dict(self):
+        return {
+            'message': self.message,
+            'errors': self.errors
+        }
